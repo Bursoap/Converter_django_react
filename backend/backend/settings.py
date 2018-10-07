a68e855d0c0d3da2a8bec54cfcb87959fbb0d972
Andrey <andrei_kulikov@icloud.com>
2018-10-08 00:55:59 +0300

    add CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,10 +27,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'localhost:3000',
+)
+
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'converter.apps.ConverterConfig',
     'django.contrib.admin',
@@ -44,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        number: '',
+        result: '',
+        errors: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+  this.setState({number: event.target.value});
+  }
+
+  handleSubmit(event) {
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            var csrftoken = getCookie('csrftoken');
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+    $.ajax({
+        url:    'http://localhost:8000/',
+        type:   'POST',
+        data:    JSON.stringify({"number": 123}),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function(response){
+            $('.invalid-feedback').hide();
+            $('.is-invalid').removeClass('is-invalid');
+            console.log(response['post']);
+            this.setState({result: response['post']});
+         }.bind(this),
+        error: function(response) {
+            var error = response["detail"];
+            this.setState({errors: response['errors']});
+            $(".number").html(error).show();
+            $("#number").addClass('is-invalid')
+        }.bind(this)
+    });
+    return false;
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <textarea value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+            <textarea value={this.state.result} readOnly></textarea>
+        </div>
     );
   }
 }
 
+function csrfSafeMethod(method) {
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = $.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 export default App;
